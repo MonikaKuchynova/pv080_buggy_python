179b96e50c249a7192d2f56e1b91563dcf951320
MonikaKuchynova <84062174+MonikaKuchynova@users.noreply.github.com>
2021-05-12 08:47:21 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -14,7 +14,7 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def foot(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
